868719b6e47c825ab1a87dcae45806d73376a45d
Bruno2407 <brunonahuelmarasi1@gmail.com>
2025-03-29 19:42:48 -0300

    terminamos

---
+++ codigo.js
@@ -0,0 +1,72 @@
+let listaTareas = []
+let ultimoNumeroTarea = 0
+
+const agregarTarea = () => {
+
+    const timestamp = Date.now()
+    const fechaActual = new Date(timestamp)
+
+    let tareaIngresada = document.getElementById("tareaIngresada").value
+
+    if(tareaIngresada != "") {
+        listaTareas.push({
+            tarea: tareaIngresada,
+            tachado: false,
+            numeroTarea: ultimoNumeroTarea++,
+            fechaCreado: fechaActual,
+            fechaTachado: ""
+        })
+    }
+
+    mostrarTareas()
+}
+
+const mostrarTareas = () => {
+
+    let contenedorDeTareas = document.getElementById("contenedorDeTareas")
+    contenedorDeTareas.innerHTML = ""
+
+    listaTareas.forEach(t => {
+
+        contenedorDeTareas = document.getElementById("contenedorDeTareas").innerHTML += `
+        <div class="tarea-texto"> 
+            <p id="tarea${t.numeroTarea}" class="tarea-nombre ${t.tachado ? "tarea-completa" : "tarea-no-completa"}">${t.tarea}</p>
+            <p class="fechaCreacion">Fecha de creacion:${t.fechaCreado}<p/>
+            <p class="fechaTachado" id="fechaTachado${t.numeroTarea}">Fecha de tachado: ${t.tachado ? t.fechaTachado : "No fue tachado"}<p/>
+        </div>
+
+        <input type="checkbox" value="${t.numeroTarea}" class="checkbox" onchange="tacharTarea(this)" ${t.tachado ? "checked" : ""}>
+        `
+    })
+}
+
+const tacharTarea = (tachado) => {
+
+    const timestamp = Date.now()
+    const fechaActual = new Date(timestamp)
+
+    let tarea = listaTareas.find(t => t.numeroTarea == tachado.value)
+    const textoTarea = document.getElementById(`tarea${tarea.numeroTarea}`)
+    const textoTareaTachado = document.getElementById(`fechaTachado${tarea.numeroTarea}`)
+
+    if (tarea.tachado == false) {
+        tarea.tachado = true
+        tarea.fechaTachado = fechaActual
+        textoTarea.classList.add("tarea-completa")
+        textoTareaTachado.innerHTML = `Fecha de tachado: ${tarea.fechaTachado}`
+    }
+    else {
+        tarea.tachado = false
+        tarea.fechaTachado = ""
+        textoTarea.classList.remove("tarea-completa")
+        textoTareaTachado.innerHTML = "No fue tachado"
+    }
+    
+}
+
+const botonBorrar = () => {
+    listaTareas = []
+    mostrarTareas()
+}
+
+
